4a718dd0ec5d395eadef60b4b351419d06668813
Kazusa Kondo <hahaha.kazusa@gmail.com>
2026-07-05 20:22:08 +0900

    コード修正

--- lib/features/map/presentation/ui/map_page.dart
+++ lib/features/map/presentation/ui/map_page.dart
@@ -6,41 +6,28 @@ import '../../../../common/constants/map_constants.dart';
 import '../providers/map_provider.dart';
 
 class MapPage extends ConsumerStatefulWidget {
-
   const MapPage({super.key});
 
   @override
-  ConsumerState<MapPage> createState() =>
-      _MapPageState();
+  ConsumerState<MapPage> createState() => _MapPageState();
 }
 
-class _MapPageState
-    extends ConsumerState<MapPage> {
-
-  final MapController _controller =
-  MapController();
+class _MapPageState extends ConsumerState<MapPage> {
+  final MapController _controller = MapController();
 
   @override
   void initState() {
-
     super.initState();
 
-    WidgetsBinding.instance
-        .addPostFrameCallback((_) async {
-
-      final vm =
-      ref.read(mapProvider.notifier);
-
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      final vm = ref.read(mapProvider.notifier);
       await vm.loadHistory();
-
       await vm.start();
-
     });
 
     ref.listenManual(
       mapProvider,
           (previous, next) {
-
         if (next.currentLocation == null) {
           return;
         }
@@ -49,93 +36,52 @@ class _MapPageState
           next.currentLocation!,
           MapConstants.currentLocationZoom,
         );
-
       },
     );
   }
 
   @override
   Widget build(BuildContext context) {
-
-    final state =
-    ref.watch(mapProvider);
+    final state = ref.watch(mapProvider);
 
     return Scaffold(
-
       appBar: AppBar(
         title: const Text("Map"),
       ),
-
       body: FlutterMap(
-
         mapController: _controller,
-
         options: MapOptions(
-
           initialCenter: LatLng(
             MapConstants.tokyoStationLat,
             MapConstants.tokyoStationLng,
           ),
-
-          initialZoom:
-          MapConstants.initialZoom,
-
-          minZoom:
-          MapConstants.minZoom,
-
-          maxZoom:
-          MapConstants.maxZoom,
-
+          initialZoom: MapConstants.initialZoom,
+          minZoom: MapConstants.minZoom,
+          maxZoom: MapConstants.maxZoom,
         ),
-
         children: [
-
           TileLayer(
-
-            urlTemplate:
-            MapConstants.tileUrl,
-
-            userAgentPackageName:
-            MapConstants.userAgent,
-
+            urlTemplate: MapConstants.tileUrl,
+            userAgentPackageName: MapConstants.userAgent,
           ),
 
           if (state.currentLocation != null)
-
             MarkerLayer(
-
               markers: [
-
                 Marker(
-
-                  point:
-                  state.currentLocation!,
-
-                  width:
-                  MapConstants.markerSize,
-
-                  height:
-                  MapConstants.markerSize,
-
+                  point: state.currentLocation!,
+                  width: MapConstants.markerSize,
+                  height: MapConstants.markerSize,
                   child: const Icon(
                     Icons.my_location,
                     color: Colors.blue,
-                    size:
-                    MapConstants.markerSize,
+                    size: MapConstants.markerSize,
                   ),
-
                 ),
-
               ],
-
             ),
-
         ],
-
       ),
-
     );
-
   }
-
 }
